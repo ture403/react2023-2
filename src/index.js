e8e91a0e7387c59f7dfd6a7b95d51ea797c7b969
ture403 <ture403@gmail.com>
2023-05-10 11:39:49 +0900

    2023-05-10

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+function Hello(props){
+  return(
+  <div>
+    <div>{props.t1}</div>
+    <div>{props.t2}</div>
+    <div>{props.t3}</div>
+    <div>{props.t4}</div>
+  </div>
+  )
+}
+
+const name = {
+  name : "webstoryboy",
+  text : "hello",
+  author : {
+    name : "baby",
+    url : "naver.com"
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Hello t1={name.name} t2={name.text} t3={name.author.name} t4={name.author.url}/>);
